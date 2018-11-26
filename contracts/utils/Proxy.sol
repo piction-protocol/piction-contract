f880decdb5a69962b185300d62cb54ddc10889a6
charls.kim <charls.kim.dev@gmail.com>
2018-11-26 18:14:39 +0900

    proxy version 구현

--- contracts/utils/Proxy.sol
+++ contracts/utils/Proxy.sol
@@ -1,18 +1,21 @@
 pragma solidity ^0.4.24;
 
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-
 import "contracts/interface/IProxy.sol";
 
-contract Proxy is IProxy, Ownable {
+contract Proxy is IProxy {
 
     function setTargetAddress(address _address) public onlyOwner {
         require(_address != address(0));
         targetAddress = _address;
+        version++;
     }
 
-    function getTargetAddress() public view returns(address) {
+    function getTargetAddress() public view returns(address targetAddresss_) {
         return targetAddress;
+    }
+
+    function getVersion() public view returns(uint256 version_) {
+        return version;
     }
 
     function () public {
