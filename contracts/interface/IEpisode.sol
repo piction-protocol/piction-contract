d089658a4bf8db8536b2c9f8cd189518714b0795
charls.kim <charls.kim.dev@gmail.com>
2018-11-26 18:13:06 +0900

    접근 제어자 수정

--- contracts/interface/IEpisode.sol
+++ contracts/interface/IEpisode.sol
@@ -5,7 +5,7 @@ import "contracts/interface/IContents.sol";
 contract IEpisode is IContents{
     function getImages() public view returns(bytes16[] images_);
     function getImage(uint256 _index) public view returns(bytes16 image_);
-    function setImage(bytes16 _image, uint256 _index) public;
-    function setImages(bytes16[] _images) public;
-    function changeImageOrder(uint256 _oldOrder, uint256 _newOrder) public;
+    function setImage(bytes16 _image, uint256 _index) external;
+    function setImages(bytes16[] _images) external;
+    function changeImageOrder(uint256 _oldOrder, uint256 _newOrder) external;
 }
